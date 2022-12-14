9c65d6ffbe3a352c2c31c0c5ae5e06a215eead43
zkgogreen <88497374+zkgogreen@users.noreply.github.com>
2022-12-14 07:52:56 +0700

    Update stream-heart.py

--- stream-heart.py
+++ stream-heart.py
@@ -7,37 +7,25 @@ st.title("Predection and Analysis of Heart Attack")
 
 col1, col2 = st.columns(2)
 
-# with col1:
-#     Pregnancies = st.text_input('Input nilai Pregnancies')
-#     Glucose = st.text_input('Input nilai Glucose')
-#     BloodPressure = st.text_input('Input nilai BloodPressure')
-#     SkinThickness = st.text_input('Input nilai SkinThickness')
-
-# with col2:
-#     Insulin = st.text_input('Input nilai Insulin')
-#     BMI = st.text_input('Input nilai BMI')
-#     DiabetesPedigreeFunction = st.text_input('Input nilai DiabetesPedigreeFunction')
-#     Age = st.text_input('Input nilai Age')
-
 with col1:
     age = st.text_input("Age")
-    sex = st.text_input("sex")
-    cp = st.text_input("cp")
-    trtbps = st.text_input("trtbps")
-    chol = st.text_input("chol")
-    fbs = st.text_input("fbs")
-    restecg = st.text_input("restecg")
+    sex = st.selectbox("sex",["Male","Female"])
+    cp = st.slider("Chest Pain type chest pain type",0,4)
+    trtbps = st.text_input("resting blood pressure (in mm Hg)")
+    chol = st.text_input("cholestoral in mg/dl fetched via BMI sensor")
+    fbs = st.checkbox("fasting blood sugar")
+    restecg = st.slider("resting electrocardiographic results",0,2)
 
 with col2:
-    thalachh = st.text_input("thalachh")
-    exng = st.text_input("exng")
-    oldpeak = st.text_input("oldpeak")
-    slp = st.text_input("slp")
-    caa = st.text_input("caa")
-    thall = st.text_input("thall")
+    thalachh = st.text_input("maximum heart rate achieved")
+    exng = st.checkbox("exercise induced angina")
+    oldpeak = st.text_input("Previous peak")
+    slp = st.text_input("Slope")
+    caa = st.slider("number of major vessels",0,3)
+    thall = st.text_input("Thal rate")
 
 diab_diagnosis = ''
-
+sex = 0 if sex == "Male" else 1
 if st.button("Predict"):
     diab_prediction = diabetes_model.predict([[
        age,sex,cp,trtbps,chol,fbs,restecg,thalachh,exng,oldpeak,slp,caa,thall
